498c649794d36016953c550e54377dadc61ba5cb
Philipp <philipp@kratzer.me>
2025-07-17 01:24:18 +0200

    Initial Structure of Media Classes

---
+++ Immersion/Classes/Scene.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immersion.Classes
+{
+    internal class Scene
+    {
+        private List<Picture> pictureList;
+        private List<Video> videoList;
+        private List<Sound> soundList;
+        public Scene() {
+            pictureList = new List<Picture>();
+            videoList = new List<Video>();
+            soundList = new List<Sound>();
+        }
+
+        public void AddPicture(Picture picture)
+        {
+            pictureList.Add(picture);
+        }
+    }
+}
